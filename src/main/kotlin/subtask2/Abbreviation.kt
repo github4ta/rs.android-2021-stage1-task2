2a58082880d91ed82bf42bbda0c5e87a9524fd36
“github4ta“ <serge.tsarik@gmail.com>
2021-06-21 11:25:05 +0300

    Add Abbreviation solution

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,33 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        if(a.length < b.length) {
+            return "NO"
+        }
+
+        val matched = mutableListOf<Char>()
+        val aUppercase = a.toUpperCase()
+        var matcher: Char = b[0]
+        var index = 0
+
+        for (element in aUppercase) {
+            if (matcher == element) {
+                matched.add(matcher)
+                index +=1
+                if(index >= b.length) {
+                    break
+                } else {
+                    matcher = b[index]
+                }
+            }
+        }
+
+        val matchedString = matched.joinToString("")
+
+        if (b == matchedString) {
+            return "YES"
+        }
+        return "NO"
     }
 }
