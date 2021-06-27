17f7a24c78b34328a35588ac1f1ee742ed7bf531
“github4ta“ <serge.tsarik@gmail.com>
2021-06-27 15:07:21 +0300

    Update Blocks task

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -7,40 +7,50 @@ import kotlin.reflect.KClass
 class Blocks {
 
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        val blockC = mutableListOf<Any>()
-        if (blockA.size <= 0) {
-            throw NotImplementedError("Array is empty")
-        }
+        val result = mutableListOf<Any>()
 
-        for (i in 0..blockA.size - 1) {
-            if (blockB.isInstance(blockA[i])) {
-                blockA[i]?.let { blockC.add(it) }
-            }
-        }
-
-        if (blockB.equals(Int::class)) {
-            return blockC.sumBy { it.toString().toInt() }
-        }
-
-        if (blockB.equals(String::class)) {
-            return blockC.joinToString("") { it.toString() }
-
-        }
-
-        if (blockB.equals(LocalDate::class)) {
-            val blockC2 = mutableListOf<LocalDate>()
-            for (i in 0..blockC.size - 1) {
-                val date = blockC.get(i).toString()
-                try {
-                    blockC2.add(LocalDate.parse(date))
-                } catch (e: DateTimeParseException){
-                    throw NotImplementedError("Date is not correct")
+        when (blockB) {
+            String::class -> {
+                for (i in blockA.indices) {
+                    if ((blockA[i] as? String) != null) {
+                        blockA[i]?.let { result.add(it) }
+                    }
                 }
+                return result.joinToString("") { it.toString() }
             }
-            val maxDate = blockC2.max()
-            return "${maxDate?.dayOfMonth}.${maxDate?.monthValue}.${maxDate?.year}"
-        }
 
-        return throw NotImplementedError("KClass<> is not Int or String, or LocalDate")
+            Int::class -> {
+                for (i in blockA.indices) {
+                    if ((blockA[i] as? Int) != null) {
+                        blockA[i]?.let { result.add(it) }
+                    }
+                }
+                return result.sumBy { it.toString().toInt() }
+            }
+
+            LocalDate::class -> {
+                for (i in blockA.indices) {
+                    if ((blockA[i] as? LocalDate) != null) {
+                        blockA[i]?.let { result.add(it) }
+                    }
+                }
+
+                val resultDate = mutableListOf<LocalDate>()
+                for (i in 0 until result.size) {
+                    val date = result[i].toString()
+                    try {
+                        resultDate.add(LocalDate.parse(date))
+                    } catch (e: DateTimeParseException){
+                        throw NotImplementedError("Date is not correct")
+                    }
+                }
+                val maxDate = resultDate.max()
+                return "${maxDate?.dayOfMonth}.${maxDate?.monthValue}.${maxDate?.year}"
+            }
+
+            else -> {
+                throw NotImplementedError("KClass<> is not Int or String, or LocalDate")
+            }
+        }
     }
 }
